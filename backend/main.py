535adc7adfde7867b334f43667751942490ab6fe
PatzPaul <82320162+PatzPaul@users.noreply.github.com>
2023-04-22 15:32:14 +0000

    Added a serializer model

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ This module contains the Flask application for our project.
 
 
 from flask import Flask
-from flask_restx import Api, Resource
+from flask_restx import Api, Resource, fields
 from config import DevConfig
 from models import Recipe
 from exts import db
@@ -16,6 +16,15 @@ db.init_app(app)
 
 api = Api(doc='/docs', app=app)
 
+# Model (serializer)
+recipe_model=api.model(
+    "Recipe",
+    {
+    "id":fields.Integer(),
+    "title":fields.String(),
+    "description":fields.String()
+    }
+)
 
 @api.route('/hello')
 class HelloResource(Resource):
@@ -30,6 +39,22 @@ class HelloResource(Resource):
             A dictionary containing a message.
         """
         return {"message": "Hello World"}
+
+
+@api.route('/recipes')
+class  RecipeResource(Resource):
+
+    """
+    A resource class that handles the various http'recipes' endpoint.
+    """
+    def get(self):
+        """get all recipes"""
+        pass
+
+    def post(self):
+        """create a new recipe"""
+
+
 
 @app.shell_context_processor
 def make_shell_context():
