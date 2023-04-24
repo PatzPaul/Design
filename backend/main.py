1d7bc3e1ba7402fb1b10166a37b1416e58959a6a
PatzPaul <82320162+PatzPaul@users.noreply.github.com>
2023-04-24 10:29:53 +0000

    Created login and sign up routes

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,13 @@ This module contains the Flask application for our project.
 """
 
 
-from flask import Flask , request
+from flask import Flask, request, jsonify
 from flask_restx import Api, Resource, fields
 from config import DevConfig
-from models import Recipe
+from models import Recipe, User
 from exts import db
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
@@ -16,25 +17,35 @@ app.config.from_object(DevConfig)
 db.init_app(app)
 
 
-migrate=Migrate(app,db)
+migrate = Migrate(app, db)
 
 api = Api(doc='/docs', app=app)
 
 # Model (serializer)
-recipe_model=api.model(
+recipe_model = api.model(
     "Recipe",
     {
-    "id":fields.Integer(),
-    "title":fields.String(),
-    "description":fields.String()
+        "id": fields.Integer(),
+        "title": fields.String(),
+        "description": fields.String()
     }
 )
+signup_model = api.model(
+    'SignUp',
+    {
+        "username": fields.String(),
+        "email": fields.String(),
+        "password": fields.String()
+    }
+)
+
 
 @api.route('/hello')
 class HelloResource(Resource):
     """
     A resource class that handles the 'hello' endpoint.
     """
+
     def get(self):
         """
         Handles a GET request to the 'hello' endpoint.
@@ -45,69 +56,97 @@ class HelloResource(Resource):
         return {"message": "Hello World"}
 
 
+@api.route('/signup')
+class Signup(Resource):
+    @api.expect(signup_model)
+    def post(self):
+        """User account sign up"""
+        data = request.get_json()
+
+        username = data.get('username')
+
+        db_user = User.query.filter_by(username=username).first()
+
+        if db_user is not None:
+            return jsonify({"message": f"User with username {username} already exists"})
+
+        new_user = User(
+            username=data.get('username'),
+            email=data.get('email'),
+            password=generate_password_hash(data.get('password'))
+        )
+        new_user.save()
+        return jsonify ({"message":"user has been created succesfully"})
+
+
+@api.route('/login')
+class Login(Resource):
+    def post(self):
+        """User authentication when logging in"""
+        pass
+
+
 @api.route('/recipes')
-class  RecipesResource(Resource):
+class RecipesResource(Resource):
     """
     A resource class that handles the various http'recipes' endpoint.
     """
     @api.marshal_list_with(recipe_model)
     def get(self):
         """get all recipes"""
-        recipes=Recipe.query.all()
+        recipes = Recipe.query.all()
 
         return recipes
 
     @api.marshal_with(recipe_model)
+    @api.expect(recipe_model)
     def post(self):
         """create a new recipe"""
         data = request.get_json()
 
-        new_recipe=Recipe(
+        new_recipe = Recipe(
             title=data.get('title'),
             description=data.get('description')
         )
         new_recipe.save()
-        return new_recipe,201
+        return new_recipe, 201
+
 
 @api.route('/recipe/<int:id>')
 class RecipeResource(Resource):
-    def get(self,id):
+    def get(self, id):
         """get a recipe by id"""
-        recipe=Recipe.query.get_or_404(id)
+        recipe = Recipe.query.get_or_404(id)
 
         return recipe
 
     @api.marshal_with(recipe_model)
-    def put(self,id):
+    def put(self, id):
         """update a recipe by id"""
-        
-        recipe_to_update=Recipe.query.get_or_404(id)
+
+        recipe_to_update = Recipe.query.get_or_404(id)
 
         data = request.json()
 
-        recipe_to_update.update(data.get(''),data.get('description'))
+        recipe_to_update.update(data.get(''), data.get('description'))
 
         return recipe_to_update
 
     @api.marshal_with(recipe_model)
-    def delete(self,id):
+    def delete(self, id):
         """delete a recipe by id"""
-        recipe_to_delete =Recipe.query.get_or_404(id)
+        recipe_to_delete = Recipe.query.get_or_404(id)
 
         return recipe_to_delete
-
-
 
 
 @app.shell_context_processor
 def make_shell_context():
     return {
-        "db":db,
-        "Recipe":Recipe
+        "db": db,
+        "Recipe": Recipe
     }
-
 
 
 if __name__ == '__main__':
     app.run()
-
