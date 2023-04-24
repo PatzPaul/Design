4ae58b1f5debe56ae8e2231294eeaf7d7e33ccc0
PatzPaul <82320162+PatzPaul@users.noreply.github.com>
2023-04-24 10:30:23 +0000

    implemented password hashing for user model

--- backend/models.py
+++ backend/models.py
@@ -44,11 +44,15 @@ class User:
     password:string
 """
 class User(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    username=db.Column(db.String(25),nullable=False)
-    email=db.Column(db.String(80),nullable=False)
-    password=db.Column(db.Text(),nullable=False)
-
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.Text(), nullable=False)
 
     def __repr__(self):
         return f"<User {self.username}>"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
