a80e7792631f72fe87cd2f69111c049b6c4a214c
PatzPaul <82320162+PatzPaul@users.noreply.github.com>
2023-04-28 10:42:28 +0000

    made first API call and useEffect

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 
 const App=()=>{
 
+  useEffect(
+    ()=>{
+      fetch('/recipe/hello')
+      .then(response=>response.json())
+      .then(data=>console.log(data))
+    },[]
+  )
+
   const [message,setMessage]=useState('')
   return (
     <div className="app">
